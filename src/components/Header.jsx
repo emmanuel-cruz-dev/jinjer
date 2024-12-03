30b8e42cde6abe15949e7a44637dde569b13e645
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-03 17:06:22 -0300

    barra lateral

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,17 @@ import { useTranslation } from "react-i18next";
 import LanguageSwitch from "./LanguageSwitch";
 import { useState, useEffect } from "react";
 import Name from "./Name";
+import SocialLinks from "./SocialLinks";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLatestAlbumOpen, setIsLatestAlbumOpen] = useState(false);
   const { t } = useTranslation();
+
+  const toggleLatestAlbum = () => {
+    setIsLatestAlbumOpen(!isLatestAlbumOpen);
+  };
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -29,16 +35,50 @@ const Header = () => {
 
   return (
     <header
-      className={`header fixed top-0 left-0 w-full transition-colors duration-300 z-50 h-20 lg:h-28 ${
+      className={`header fixed top-0 left-0 w-full flex transition-colors duration-300 z-50 h-20 lg:h-28 ${
         isScrolled ? "bg-background" : "bg-transparent"
       }`}
     >
-      <nav className="header__navbar flex justify-between items-center uppercase max-w-[1200px] mx-auto h-full px-8">
+      <article
+        className={`latest-album z-10 ${isLatestAlbumOpen ? "active" : ""}`}
+      >
+        <div className="flex items-center">
+          <span
+            onClick={() => toggleLatestAlbum()}
+            className="material-symbols-outlined text-3xl cursor-pointer transition-transform duration-300 hover:rotate-90"
+          >
+            close
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 px-4 text-center">
+          <h2 className="text-2xl uppercase font-bold">Latest Album</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus
+            risus mauris, et commodo lectus hendrerit ac.
+          </p>
+          <span className="text-2xl">
+            <SocialLinks />
+          </span>
+        </div>
+      </article>
+      <nav
+        className={`header__navbar ${
+          isLatestAlbumOpen ? "translate-x" : ""
+        }  flex justify-between items-center gap-4 uppercase mx-auto h-full px-8`}
+      >
         <span
           onClick={() => toggleMenu()}
-          className="material-symbols-outlined text-3xl"
+          className="material-symbols-outlined text-3xl lg:hidden cursor-pointer"
         >
           {isMenuOpen ? "close" : "menu"}
+        </span>
+
+        <span
+          onClick={() => toggleLatestAlbum()}
+          className="material-symbols-outlined hidden text-3xl lg:block cursor-pointer"
+        >
+          queue_music
         </span>
 
         <ul className={`header__menu-list ${isMenuOpen ? "active" : "hidden"}`}>
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,46 @@
+import {
+  FaSpotify,
+  FaSoundcloud,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa6";
+const SocialLinks = () => {
+  return (
+    <div className="social-links flex gap-4">
+      <a
+        href="https://youtube.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Youtube"
+      >
+        <FaYoutube />
+      </a>
+      <a
+        href="https://soundcloud.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Soundcloud"
+      >
+        <FaSoundcloud />
+      </a>
+      <a
+        href="https://spotify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Spotify"
+      >
+        <FaSpotify />
+      </a>
+      <a
+        href="https://instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Instagram"
+      >
+        <FaInstagram />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
