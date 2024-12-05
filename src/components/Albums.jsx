8af32711f8361c4a594d43b1e173a21b5b2c28bf
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-05 15:28:15 -0300

    albums agregados y renderizados con componente

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,22 +1,28 @@
 import AlbumCover01 from "../assets/images/album-cover-01.jpg";
+import AlbumCover02 from "../assets/images/album-cover-02.jpg";
+import AlbumCover03 from "../assets/images/album-cover-03.jpg";
+import AlbumCover04 from "../assets/images/album-cover-04.jpg";
+import AlbumCover05 from "../assets/images/album-cover-05.jpg";
+import AlbumCover06 from "../assets/images/album-cover-06.jpg";
 import VinylDisc01 from "../assets/images/vinyl-disc-01.png";
 
-const Album = () => {
+const Album = ({ cover, title, year }) => {
   return (
     <article className="album__container">
       <div className="album__images__container">
-        <img className="album__cover" src={AlbumCover01} alt="" />
+        <img className="album__cover" src={cover} alt="" />
         <img className="album__disc" src={VinylDisc01} alt="" />
       </div>
-      <h3 className="album__title">Royal Blood</h3>
-      <span className="album__year">2023</span>
+      <h3 className="album__title">{title}</h3>
+      <span className="album__year">{year}</span>
     </article>
   );
 };
+
 const Albums = () => {
   return (
     <section
-      className="scroll-top-margin albums py-16 mx-auto bg-white text-black"
+      className="scroll-top-margin albums py-16 mx-auto bg-slate-300 text-black"
       id="albums"
     >
       <article className="flex flex-col justify-center items-center gap-12 w-11/12 mx-auto">
@@ -29,10 +35,13 @@ const Albums = () => {
             forget subscribe us!
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-24 md:flex-row w-full xl:w-5/6">
-          <Album />
-          <Album />
-          <Album />
+        <div className="grid grid-cols-2 md:grid-cols-3 justify-items-center gap-x-32 gap-y-12 w-full xl:w-5/6">
+          <Album cover={AlbumCover01} title="Royal Blood" year="2024" />
+          <Album cover={AlbumCover02} title="Bird of Hermes" year="2021" />
+          <Album cover={AlbumCover03} title="Vertical" year="2019" />
+          <Album cover={AlbumCover04} title="Ramones" year="2017" />
+          <Album cover={AlbumCover05} title="Afterlife" year="2014" />
+          <Album cover={AlbumCover06} title="The Jam" year="2011" />
         </div>
       </article>
     </section>
