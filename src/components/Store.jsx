6956347595d22b97ba1f3b681772b8636d21318d
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-06 11:59:22 -0300

    remeras agregadas

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,11 +1,15 @@
 import { useTranslation } from "react-i18next";
-import TShirt1 from "../assets/images/shop-01.png";
+import TShirt1 from "../assets/images/shop-afterlife-black.png";
+import TShirt2 from "../assets/images/shop-bird-black.png";
+import TShirt3 from "../assets/images/shop-bird-white.png";
+import TShirt4 from "../assets/images/shop-official-logo-black.png";
+import TShirt5 from "../assets/images/shop-official-logo-white.png";
 
-const TShirtCard = ({ title, price }) => {
+const TShirtCard = ({ title, price, img }) => {
   return (
     <div className="relative">
       <figure className="">
-        <img className="" src={TShirt1} alt="" />
+        <img className="" src={img} alt="" />
       </figure>
       <div className="flex justify-around">
         <h3 className="font-bold">{title}</h3>
@@ -37,10 +41,27 @@ const Store = () => {
           <p className="text-lg">{t("store.description")}</p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-x-10 gap-y-12 w-full xl:w-11/12">
-          <TShirtCard title="T-Shirt Official Logo" price="21.99" />
-          <TShirtCard title="T-Shirt Album" price="18.69" />
-          <TShirtCard title="T-Shirt Skull" price="24.79" />
-          <TShirtCard title="T-Shirt New Tour" price="23.95" />
+          <TShirtCard title="T-Shirt Afterlife" price="21.99" img={TShirt1} />
+          <TShirtCard
+            title="T-Shirt Bird of Hermes"
+            price="18.69"
+            img={TShirt2}
+          />
+          <TShirtCard
+            title="T-Shirt Bird of Hermes/W"
+            price="24.79"
+            img={TShirt3}
+          />
+          <TShirtCard
+            title="T-Shirt Official Logo"
+            price="23.95"
+            img={TShirt4}
+          />
+          <TShirtCard
+            title="T-Shirt Official Logo/W"
+            price="23.95"
+            img={TShirt5}
+          />
         </div>
         <a href="#" className="red__btn">
           {t("store.amazonButton")}
