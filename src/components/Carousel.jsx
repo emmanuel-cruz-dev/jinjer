75835bfe3b4a8b09ac152bb85080726fcaef5931
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-11 15:32:28 -0300

    imprimiendo tarjetas about dinamicamente

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -11,57 +11,57 @@ import CardImg3 from "../assets/images/blog-card-03.jpg";
 const articles = [
   {
     id: 1,
-    title: "Artículo 1",
-    content: "Contenido completo del artículo 1...",
+    title: "Neuer Song: Ein Naomi Hoffnung",
+    image: CardImg1,
+    date: "February 23,2023",
+    description:
+      "Naomi, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
   },
   {
     id: 2,
-    title: "Artículo 2",
-    content: "Contenido completo del artículo 2...",
+    title: "Neue Benjamin Ep in arbeit",
+    image: CardImg2,
+    date: "February 11,2013",
+    description:
+      "Benjita, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
   },
   {
     id: 3,
-    title: "Artículo 3",
-    content: "Contenido completo del artículo 3...",
+    title: "Zeitreise - Evolution von 96 July",
+    image: CardImg3,
+    date: "February 3,2008",
+    description:
+      "July, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
+  },
+  {
+    id: 4,
+    title: "Song 4: Ein Naomi Hoffnung",
+    image: CardImg1,
+    date: "February 23,2023",
+    description:
+      "Naomi, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
+  },
+  {
+    id: 5,
+    title: "Neue 5 Benjamin Ep in arbeit",
+    image: CardImg2,
+    date: "February 11,2013",
+    description:
+      "Benjita, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
+  },
+  {
+    id: 6,
+    title: "Zeitreise #06 - Evolution von July",
+    image: CardImg3,
+    date: "February 3,2008",
+    description:
+      "July, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat tempora. Expedita voluptatem maxime a!",
   },
 ];
 
-const ArticleCard = ({ image, title, date }) => {
-  const { id } = useParams();
-  const article = articles.find((article) => article.id === id);
-  return (
-    <div className="latest-news__card flex flex-col w-[22rem] gap-3">
-      <figure className="overflow-hidden">
-        <img
-          className="scale-105 -rotate-3 hover:scale-100 hover:rotate-0 transition-transform duration-300"
-          src={image}
-          alt=""
-        />
-      </figure>
-      <Link to="/newsblog">
-        <h3 className="uppercase font-bold tracking-wide text-lg opacity-80 hover:opacity-100 transition-opacity duration-300">
-          {title}
-        </h3>
-      </Link>
-      <p className="text-gray-400">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, quaerat
-        tempora. Expedita voluptatem maxime a!
-      </p>
-      <a
-        className="uppercase hover:underline transition-all duration-300 ease-in-out"
-        href="#"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Read more
-      </a>
-      <time className="text-sm uppercase" dateTime="2018-01-16">
-        {date}
-      </time>
-    </div>
-  );
-};
+const ArticleCard = () => {};
 
+const div = () => {};
 const Carousel = () => {
   const settings = {
     dots: false, // Mostrar puntos de navegación
@@ -93,45 +93,41 @@ const Carousel = () => {
   return (
     <div style={{ padding: "12px" }}>
       <Slider {...settings} className="latest-news__carousel-slider">
-        <div>
-          {/* Card 1 */}
-          <ArticleCard
-            image={CardImg1}
-            title="Neuer Song: Ein Naomi Hoffnung"
-            date="9 Nov 2020"
-          />
-        </div>
-
-        <div>
-          {/* Card 2 */}
-          <ArticleCard
-            image={CardImg2}
-            title="Neue Benjamin Ep in arbeit"
-            date="11 Feb 2013"
-          />
-        </div>
-
-        <div>
-          {/* Card 3 */}
-          <ArticleCard
-            image={CardImg3}
-            title="Zeitreise - Evolution von 96 July"
-            date="3 Feb 2008"
-          />
-        </div>
-
-        <div>
-          {/* Card 4 */}
-          <ArticleCard image={CardImg} date="16 Jan 2018" />
-        </div>
-        <div>
-          {/* Card 5 */}
-          <ArticleCard image={CardImg} date="16 Jan 2018" />
-        </div>
-        <div>
-          {/* Card 6 */}
-          <ArticleCard image={CardImg} date="16 Jan 2018" />
-        </div>
+        {articles.map((article) => {
+          return (
+            <div
+              key={article.id}
+              className="latest-news__card flex flex-col w-[22rem] gap-3"
+            >
+              <figure className="overflow-hidden">
+                <img
+                  className="scale-105 -rotate-3 hover:scale-100 hover:rotate-0 transition-transform duration-300"
+                  src={article.image}
+                  alt=""
+                />
+              </figure>
+              <Link to="/newsblog">
+                <h3 className="uppercase font-bold tracking-wide text-lg opacity-80 hover:opacity-100 transition-opacity duration-300">
+                  {article.title}
+                </h3>
+              </Link>
+              <p className="text-gray-400">{article.description}</p>
+              <div className="flex flex-col gap-2">
+                <a
+                  className="uppercase hover:underline transition-all duration-300 ease-in-out"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read more
+                </a>
+                <time className="text-sm uppercase" dateTime="2018-01-16">
+                  {article.date}
+                </time>
+              </div>
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
