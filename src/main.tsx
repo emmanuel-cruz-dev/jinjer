d3f420b0047d3500253cfc2448059b7fd43c1252
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-20 08:21:53 -0300

    cambiando main y App a tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import "./styles/main.scss";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
       <App />
