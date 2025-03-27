7ed97a5c683c043678c72b6c05018ffe2ab85934
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-27 09:22:17 -0300

    feat:useCart hook agregado

--- src/context/cart.tsx
+++ src/context/cart.tsx
@@ -12,8 +12,17 @@ export function CartProvider({ children }) {
     if (productInCartIndex >= 0) {
       const newCart = structuredClone(cart);
       newCart[productInCartIndex].quantity += 1;
-      setCart(newCart);
+      return setCart(newCart);
     }
+
+    // Si el producto no está en el carrito
+    setCart((prevState) => [
+      ...prevState,
+      {
+        ...product,
+        quantity: 1,
+      },
+    ]);
   };
 
   const clearCart = () => {
