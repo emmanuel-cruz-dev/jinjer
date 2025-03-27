3ee473713a7e6fab269786277ba339ece30d2f3b
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-27 09:12:41 -0300

    feat: agregando cartContext

---
+++ src/context/cart.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    // Checkear si el producto ya esta en el carrito
+    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
+
+    if (productInCartIndex >= 0) {
+      const newCart = structuredClone(cart);
+      newCart[productInCartIndex].quantity += 1;
+      setCart(newCart);
+    }
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
