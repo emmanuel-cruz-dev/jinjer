0b4d92b48e9ce969e6e1a8d8b93f3ff4acac59f8
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-28 13:53:01 -0300

    style: estilos en Store productos y botones

--- src/pages/Home/Store/StoreCartItem.tsx
+++ src/pages/Home/Store/StoreCartItem.tsx
@@ -3,12 +3,11 @@ import { useCart } from "../../../hooks/useCart";
 import { useTranslation } from "react-i18next";
 import useStoreNavigation from "../../../hooks/useStoreNavigation";
 
-const StoreCartItem = () => {
+function StoreCartItem() {
   const { t } = useTranslation();
-  const { totalProductsInCart, cart } = useCart();
+  const { totalProductsInCart } = useCart();
   const isProductInCart = totalProductsInCart();
   const goToSection = useStoreNavigation();
-  console.log(cart);
 
   return (
     <article className="absolute top-0 right-0">
@@ -16,7 +15,7 @@ const StoreCartItem = () => {
         <a
           onClick={goToSection}
           href="/shop/"
-          className="bg-black text-white flex justify-center items-center hover:bg-red-500 hover:text-white text-xl w-14 h-14 rounded-full border-none focus:outline-none transition-colors duration-300 ease-in-out"
+          className="bg-black text-white flex justify-center items-center hover:bg-red-600 hover:text-white text-xl w-14 h-14 rounded-full border-none focus:outline-none transition-colors duration-300 ease-in-out"
           title={t("store.storeButton")}
         >
           <FaShoppingCart />
@@ -27,6 +26,6 @@ const StoreCartItem = () => {
       )}
     </article>
   );
-};
+}
 
 export default StoreCartItem;
--- src/pages/Home/Store/TShirtCard.tsx
+++ src/pages/Home/Store/TShirtCard.tsx
@@ -1,9 +1,8 @@
-import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { TShirtCardProps } from "../../../types/types";
 import { useCart } from "../../../hooks/useCart";
+import { ShopCardProps } from "../../../types/types";
 
-const TShirtCard = ({ product }) => {
+const TShirtCard = ({ product }: ShopCardProps) => {
   const { t } = useTranslation();
   const { addToCart } = useCart();
 
@@ -25,7 +24,7 @@ const TShirtCard = ({ product }) => {
       </div>
       <button
         onClick={() => addToCart(product)}
-        className="absolute top-2/3 right-24 lg:right-4 bg-red-600 px-2 py-1 text-sm text-white font-semibold hover:text-white hover:bg-red-700 transition-colors duration-300"
+        className="absolute top-2/3 right-24 lg:right-4 bg-red-600 px-2 py-1 text-sm text-white font-semibold rounded-none hover:text-white hover:bg-red-700 transition-colors duration-300"
       >
         {t("store.addToCart")}
       </button>
